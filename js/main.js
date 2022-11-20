345e440cd186584292e7a9de242cec0dbf240005
ibroxm0116 <ibrohimrustamov78@gmail.com>
2022-11-20 16:54:12 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// var elBody = document.querySelector('body')
+// var elModeBtn = document.querySelector('.mode-btn')
+
+// elModeBtn.addEventListener('click', function () {
+//     elBody.classList.toggle('dark');
+// })
+
+var elBody = document.querySelector('body')
+var elModeBtn = document.querySelector('.mode-btn')
+
+elModeBtn.addEventListener('click', function () {
+    elBody.classList.toggle('dark');
+})
+
